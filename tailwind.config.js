f191cb0bc0b67d48f433a345072c2f4684e5d216
meltoh <bryanyokpe@yahoo.com>
2025-01-15 20:20:17 +0300

    Updated logo and fixed missing file issues

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: false, // Disable dark mode completely
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
+
+
